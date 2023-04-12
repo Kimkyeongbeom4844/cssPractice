4ccb47055604816c1841c49b16f83b4849fffffb
kimKyeongbeom <bibix321@naver.com>
2023-04-12 17:59:36 +0900

    canvas - 지우개 기능 및 색 변경 기능 추가

--- script/app.js
+++ script/app.js
@@ -1,63 +1,56 @@
-const showDialogButton = document.querySelector(".showDialogButton");
-const dialog = document.querySelector(".dialog");
-const canvasRef = document.querySelector(".canvasRef");
-const ctx = canvasRef.getContext("2d");
-console.log(ctx);
+window.onload = () => {
+  const showDialogButton = document.querySelector(".showDialogButton");
+  const dialog = document.querySelector(".dialog");
+  const canvasRef = document.querySelector(".canvasRef");
+  const brushColor = document.querySelector(".brushColor");
+  const eraserButton = document.querySelector(".eraserButton");
+  const drawButton = document.querySelector(".drawButton");
 
-// ctx.strokeStyle = "red";
-// ctx.lineWidth = 1;
-ctx.beginPath();
-ctx.moveTo(10, 10);
-ctx.lineTo(50, 10);
-ctx.stroke();
+  const ctx = canvasRef.getContext("2d");
+  console.log(ctx);
 
-ctx.globalCompositeOperation = "destination-out";
-ctx.lineWidth = 2;
-ctx.beginPath();
-ctx.moveTo(10, 10);
-ctx.lineTo(50, 10);
-ctx.stroke();
-ctx.lineWidth = 1;
-ctx.globalCompositeOperation = "source-over";
+  ctx.strokeStyle = brushColor.value;
+  ctx.fillStyle = "green";
+  ctx.fillText("직접 구현한 캔버스", 60, 60);
 
-// ctx.strokeStyle = "blue";
-// ctx.beginPath();
-// ctx.moveTo(20, 20);
-// ctx.lineTo(50, 20);
-// ctx.stroke();
+  let mousedown = false;
+  canvasRef.addEventListener("mousedown", (e) => {
+    // console.log(
+    //   e.pageX - canvasRef.offsetLeft,
+    //   e.pageY - canvasRef.offsetTop
+    // );
+    mousedown = true;
+    ctx.beginPath();
+    ctx.moveTo(e.pageX - canvasRef.offsetLeft, e.pageY - canvasRef.offsetTop);
+  });
+  canvasRef.addEventListener("mousemove", (e) => {
+    if (mousedown) {
+      ctx.lineTo(e.pageX - canvasRef.offsetLeft, e.pageY - canvasRef.offsetTop);
+      ctx.stroke();
+    }
+    return;
+  });
+  canvasRef.addEventListener("mouseup", () => {
+    mousedown = false;
+  });
+  brushColor.addEventListener("change", (e) => {
+    ctx.strokeStyle = e.currentTarget.value;
+  });
+  drawButton.addEventListener("click", () => {
+    ctx.globalCompositeOperation = "source-over";
+  });
+  eraserButton.addEventListener("click", () => {
+    ctx.globalCompositeOperation = "destination-out";
+  });
 
-// ctx.fillStyle = "green";
-// ctx.fillText("직접 구현한 캔버스", 60, 60);
-
-let mousedown = false;
-canvasRef.addEventListener("mousedown", (e) => {
-  // console.log(
-  //   e.pageX - canvasRef.offsetLeft,
-  //   e.pageY - canvasRef.offsetTop
-  // );
-  mousedown = true;
-  ctx.beginPath();
-  ctx.moveTo(e.pageX - canvasRef.offsetLeft, e.pageY - canvasRef.offsetTop);
-});
-
-canvasRef.addEventListener("mousemove", (e) => {
-  if (mousedown) {
-    ctx.lineTo(e.pageX - canvasRef.offsetLeft, e.pageY - canvasRef.offsetTop);
-    ctx.stroke();
-  }
-  return;
-});
-canvasRef.addEventListener("mouseup", () => {
-  mousedown = false;
-});
-
-showDialogButton.addEventListener("click", () => {
-  dialog.showModal();
-});
-dialog.addEventListener("close", () => {
-  console.log(
-    `모달닫힘, 리턴값 : ${
-      dialog.returnValue === "" ? "없음" : dialog.returnValue
-    }`
-  );
-});
+  showDialogButton?.addEventListener("click", () => {
+    dialog.showModal();
+  });
+  dialog?.addEventListener("close", () => {
+    console.log(
+      `모달닫힘, 리턴값 : ${
+        dialog.returnValue === "" ? "없음" : dialog.returnValue
+      }`
+    );
+  });
+};
